22f5bee6c15162276bd636c4bdbc2dd1d2cc0dac
João Carlos Guedes Jr <80357180+JoaoCGuedesJr@users.noreply.github.com>
2021-05-24 09:49:03 -0300

    Desafio 1

---
+++ desafio.js
@@ -0,0 +1,9 @@
+/*
+    Crie 2 arquivos JS.
+    O primeiro, irá exportar uma função chamada getFlag() que receberá um argumento do tipo string.  
+    Essa função deverá buscar dentro do arrau process.argv a flag desejada, que é a string e retornao valor da flag.
+
+    O segundo irá importar a função e passar a flag desejada.  
+    Iremos rodar no terminal o segundo arquivo passando as flags --name e --greeting para que seja impresso no terminal a saudação e o nome da pessoa. 
+
+*/
